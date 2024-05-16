cdcac3c8bcadb21a84e05f940804cbbe7b349129
SekiJoWo <shihcheng0201@gmail.com>
2024-05-16 14:30:44 +0800

    first

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+<template>
+    <NuxtLoadingIndicator />
+    <NuxtLayout>
+        <NuxtPage />
+    </NuxtLayout>
+</template>
+<style>
+    html,
+    body,
+    #__nuxt {
+        height: 100vh;
+        margin: 0;
+        padding: 0;
+        background: #000000;
+    }
+</style>
---
+++ stores/state.js
@@ -0,0 +1,11 @@
+import { defineStore } from 'pinia';
+
+export const useStateStore = defineStore('state', () => {
+    const State = reactive({
+        area: 'bns',
+    });
+
+    return {
+        State,
+    };
+});
