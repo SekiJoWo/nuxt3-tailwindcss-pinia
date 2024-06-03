bef95d2dc878ed35a9419605657cf3f56d0a2c1d
SekiJoWo <shihcheng0201@gmail.com>
2024-06-03 14:29:23 +0800

    新增local.env,pages,layouts

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <p>Some default layout content shared across all pages</p>
+        <slot />
+    </div>
+</template>
+
+<!-- other -->
+<!-- 
+<script setup lang="ts">
+    definePageMeta({
+        layout: 'custom'
+    })
+</script> 
+-->
+
+<!-- pages -->
+
+<!-- 
+<script setup lang="ts">
+    const layout = 'custom';
+</script>
+
+<template>
+    <NuxtLayout :name="layout">
+        <NuxtPage />
+    </NuxtLayout>
+</template> 
+-->
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    runtimeConfig: {
+        public: {
+            ApiBase: '/api/',
+        },
+    },
     modules: ['@pinia/nuxt', '@nuxt/ui', '@pinia-plugin-persistedstate/nuxt'],
 });
